3d37270fde7e00ce887291fe383262224d4ae02a
Seth Rylan Gainey <sethrylan@github.com>
2022-01-27 09:12:01 -0500

    Make global.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/env zsh
 
 source ./zsh/zshrc.zsh
-git config --local merge.conflictStyle zdiff3
-git config --local alias.s status
-git config --local alias.co checkout
-git config --local alias.lol "log --graph --oneline --decorate"
-git config --local alias.ll "log --pretty=format:\"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn] %Cgreen%cr\" --decorate --numstat"
+git config --global merge.conflictStyle zdiff3
+git config --global alias.s status
+git config --global alias.co checkout
+git config --global alias.lol "log --graph --oneline --decorate"
+git config --global alias.ll "log --pretty=format:\"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn] %Cgreen%cr\" --decorate --numstat"
