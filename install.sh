af92747f42fc287c2e7b0d3af45e48611118894a
Seth Rylan Gainey <sethrylan@github.com>
2022-04-21 09:17:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ git config --global commit.gpgsign true
 git config --global merge.conflictStyle diff3
 git config --global alias.s status
 git config --global alias.co checkout
+git config --global pull.ff only
 git config --global alias.lol "log --graph --oneline --decorate"
 git config --global alias.ll "log --pretty=format:\"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn] %Cgreen%cr\" --decorate --numstat"
