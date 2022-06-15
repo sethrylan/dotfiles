8c1513825f03bd97e0837d2537c97c7b3f0eb605
Seth Rylan Gainey <sethrylan@github.com>
2022-06-15 17:23:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,20 @@
 dir=$(dirname "$0")
 DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles/
 
-ln -s $DOTFILES/gitconfig ~/.gitconfig
+git config --global core.trustctime false
+git config --global core.ignorecase false
+git config --global alias.s status
+git config --global alias.co checkout
+git config --global alias.lol 'log --graph --oneline --decorate'
+git config --global alias.ll 'log --pretty=format:\"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn] %Cgreen%cr\" --decorate --numstat'
+git config --global alias.lnc 'log --pretty=format:\"%h %s [%cn]\"'
+git config --global alias.lg 'log --color --graph --pretty=format:\"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset\" --abbrev-commit'
+git config --global pull.rebase false
+git config --global checkout.workers  0
+git config --global commit.gpgsign true
+git config --global merge.conflictStyle diff3
+git config --global gpg.program "/.codespaces/bin/gh-gpgsign"
+
 
 cat > ~/.zshenv <<EOF
 source $DOTFILES/zsh/env.zsh
@@ -14,6 +27,3 @@ cat > ~/.zshrc <<EOF
 source $DOTFILES/zsh/zshrc.zsh
 EOF
 
-git config --global commit.gpgsign true
-git config --global merge.conflictStyle diff3
-git config --global gpg.program "/.codespaces/bin/gh-gpgsign"
